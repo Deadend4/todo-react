9c1f32ea17c83cb909bfdd2c92d3c5d3dc7822a0
Саня <70769398+Deadend4@users.noreply.github.com>
2024-02-14 12:15:32 +0300

    add list component

--- src/App.js
+++ src/App.js
@@ -11,18 +11,26 @@ export default function App() {
   );
 }
 
-function CreateItem(array) {
+function List(list) {
   const listId = useRef(0);
-  listId.current++;
-  const item = array.length > 0 ? array[array.length - 1] : false;
-  return (
-    <li key={listId.current}>{item}</li>
-  );
+  console.log(list.list);
+  if (Array.isArray(list.list)) {
+    const listItems = list.list.map(item =>
+        <li key={listId.current}>{item}</li>
+      );
+    listId.current++;
+      return (
+        <ul>
+          <li>{listItems}</li>
+        </ul>
+      );
+  }
+      
 }
 
 function Input() {
   const [list, setList] = useState([]);
-  // const [filter, setFilter] = useState('all');
+  const [filter, setFilter] = useState('all');
   const { register, resetField, handleSubmit } = useForm();
 
   function handleInputClick(item) {
@@ -41,10 +49,7 @@ function Input() {
       <form className='input__form' onSubmit={handleSubmit(handleInputClick)}>
         <input className='input__input' placeholder='What needs to be done?' {...register('input', {required: true})} />
       </form>
-      <ul>
-        <CreateItem lastItem={list.list} />
-      </ul>
-      
+      <List list={list} />
       
     </div>
     
