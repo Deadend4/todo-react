cb034d473545e1b5464c31850f513ae03632f7ba
Саня <70769398+Deadend4@users.noreply.github.com>
2024-02-14 12:04:16 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Input />
     </div>
   );
 }
 
-export default App;
+function CreateItem(array) {
+  const listId = useRef(0);
+  listId.current++;
+  const item = array.length > 0 ? array[array.length - 1] : false;
+  return (
+    <li key={listId.current}>{item}</li>
+  );
+}
+
+function Input() {
+  const [list, setList] = useState([]);
+  // const [filter, setFilter] = useState('all');
+  const { register, resetField, handleSubmit } = useForm();
+
+  function handleInputClick(item) {
+    setList(
+      [
+        item.input,
+        ...list
+      ]
+    );
+    resetField('input');    
+  }
+
+  return (
+    <div className='input'>
+      <h1 className='input__heading'>todos</h1>
+      <form className='input__form' onSubmit={handleSubmit(handleInputClick)}>
+        <input className='input__input' placeholder='What needs to be done?' {...register('input', {required: true})} />
+      </form>
+      <ul>
+        <CreateItem lastItem={list.list} />
+      </ul>
+      
+      
+    </div>
+    
+  )
+}
+
+
