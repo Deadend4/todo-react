7b544b4d03e2eb15142a9de2071604e8e199897e
Саня <70769398+Deadend4@users.noreply.github.com>
2024-02-20 15:44:06 +0300

    add logic for changing an item

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -5,7 +5,25 @@ export default function List({
   onItemCrossClick,
   onItemCheckboxClick,
   onItemTextChange,
+  onTextClick,
 }) {
+  const startTextEditing = (e, item) => {
+    if (item.completed) {
+      e.target.style.textDecorationLine = 'none';
+    }
+    onTextClick({...item, readOnly: false});
+  }
+
+  const stopTextEditing = (e, item) => {
+    onItemTextChange({ ...item, value: e.target.value, readOnly: true });
+    if (!e.target.value) {
+      e.target.value = item.value;
+    }
+    if (item.completed) {
+      e.target.style.textDecorationLine = 'line-through';
+    }
+  }
+
   if (list.length > 0) {
     const listItems = list.map((item) => (
       <li key={item.id} className={styles.listItem}>
@@ -24,16 +42,17 @@ export default function List({
         ></label>
         <input
           className={styles.text}
-          onBlur={(e) => {
-            onItemTextChange({ ...item, value: e.target.value });
-            if (!e.target.value) {
-              e.target.value = item.value;
+          onKeyDown={(e) =>{
+            if (e.key === 'Enter') {
+              stopTextEditing(e, item);
             }
           }}
-          onTransitionEnd={(e) => {
-            e.target.style.textDecorationLine = item.completed ? 'line-through' : 'none';
-          }}
+          onBlur={(e) => stopTextEditing(e, item)}
+          onTouchStart={(e) => startTextEditing(e, item)}
+          onDoubleClick={(e) => startTextEditing(e, item)}
+          onTransitionEnd={(e) => e.target.style.textDecorationLine = item.completed ? 'line-through' : 'none'}
           defaultValue={item.value}
+          readOnly={item.readOnly}
         />
         <button
           type="button"
