1f3b34812ab1a41af19f1d377f7149f4ca91f233
Саня <70769398+Deadend4@users.noreply.github.com>
2024-02-21 20:20:48 +0300

    fix children in tooltip component

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -1,18 +1,15 @@
 import { Children } from 'react';
 import styles from './Tooltip.module.css';
 
-export default function Tooltip({message, canShow, children}) {
+export default function Tooltip({ message, canShow, children }) {
     const tooltipClasses = `${styles.tooltipText} ${canShow ? styles.tooltipTextVisible : styles.tooltipTextHidden}`
-    const mappedChildren = Children.map(children, child =>
-          child
-      );
     return (
-            <>
-                <span className={tooltipClasses}>{message}</span>
-                {mappedChildren}
-            </>
-            
-        
+        <>
+            <span className={tooltipClasses}>{message}</span>
+            {children}
+        </>
+
+
     );
-    
+
 }
